f32e76c11c40d295877a73ddc5be125ee238e250
Héctor <hector.vinas@institutvidreres.cat>
2024-12-04 13:42:14 +0100

    Initial Commit (All done)

---
+++ app/src/main/java/com/example/bottom_menu_fragments/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.bottom_menu_fragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        setupWithNavController(bottomNavView, navController)
+    }
+}
